4edc61f24932357d6a4e065fdb0e82db5dd9d536
Eric Rafael <rafaelpsec@gmail.com>
2023-09-03 17:22:28 -0300

    all routes working but without controllers

--- src/app.js
+++ src/app.js
@@ -60,43 +60,113 @@ app.get("/poll", async (req, res) => {
     }
 })
 
+app.get("/poll/:id/choice", async (req, res) => {
+    const id = req.params.id
+
+    try {
+
+        const choices = await db.collection("choices").find({ pollId: new ObjectId(id) }).toArray();
+        res.send(choices);
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+    
+})
+
+app.get("/poll/:id/result", async (req, res) => {
+    const pollId = req.params.id;
+    const listResult = []
+    let moreVotes = null;
+
+    function add(title, votes) {
+        const addChoice = {
+            title: title,
+            votes: votes
+        }
+        listResult.push(addChoice)
+        
+    }
+
+    try {
+        const poll = await db.collection("polls").findOne({ _id: new ObjectId(pollId) });
+        const choices = await db.collection("choices").find({ pollId: new ObjectId(pollId)}).toArray();
+
+        await Promise.all(choices.map(async (choice) => {
+            
+            const votes = await db.collection("votes").find({ choiceId: choice._id}).toArray();
+
+            add(choice.title, votes.length)
+            
+        }))
+
+        for (const i of listResult) {
+            if (moreVotes === null || i.votes > moreVotes.votes) {
+                moreVotes = i;
+            }
+        }
+
+        poll.result = moreVotes
+        res.status(201).send(poll)
+
+    } catch (err) {
+        res.sendStatus(404)
+    }
+})
+
 app.post("/choice", async (req, res) => {
 
     const { title, pollId } = req.body;
+    const objPollId = new ObjectId(pollId)
     
     try {
+        const today = dayjs()
+        const dateFormated = today.format('YYYY-MM-DD HH:mm');
+
         const checkPoll = await db.collection("polls").findOne({ _id: new ObjectId(pollId) });
         const checkTitle = await db.collection("choices").findOne({ title, pollId });
+
         if (checkPoll == null) {
             return res.sendStatus(404)
         } else if (title == '') {
             return res.sendStatus(422)
         } else if (checkTitle){
             return res.sendStatus(409)
+        } else if (checkPoll.expireAt<dateFormated) {
+            return res.status(403).send('Poll expirada')
         }
         
-        
-        await db.collection("choices").insertOne({ title, pollId });
-        res.sendStatus(201);
+        await db.collection("choices").insertOne({ title, pollId: objPollId });
+        const choice = await db.collection("choices").findOne({ title, pollId: objPollId })
+        res.status(201).send(choice);
 
     } catch (err) {
         res.status(500).send(err.message)
     }
 })
 
-app.get("/poll/:id/choice", async (req, res) => {
-    const id = req.params.id
+app.post("/choice/:id/vote", async (req,res) => {
+    const choiceId = req.params.id;
+    const today = dayjs();
+    const dateFormated = today.format('YYYY-MM-DD HH:mm');
 
-try {
+    try {
+        const checkChoice = await db.collection("choices").findOne({ _id: new ObjectId(choiceId) });
+        const checkPoll = await db.collection("polls").findOne({ _id: new ObjectId(checkChoice.pollId) });
 
-    const choices = await db.collection("choices").find({ pollId: id }).toArray();
-    res.send(choices);
+        if (checkPoll.expireAt<dateFormated) {
+            return res.sendStatus(403)
+        }
 
-} catch (err) {
-    res.sendStatus(404)
-}
-    
+        await db.collection("votes").insertOne({ createdAt: dateFormated, choiceId: new ObjectId(choiceId) });
+        res.sendStatus(201)
+
+    } catch (err) {
+        res.sendStatus(404)
+    }
 })
+
+
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}!`))
